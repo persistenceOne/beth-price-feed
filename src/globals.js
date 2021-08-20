55a38544846df1fcd025d18fbbae777be0a0ddf4
Bogdan Kovtun <psirex48@gmail.com>
2021-08-20 03:39:41 +0300

    Parse new ENV variable to globals

--- src/globals.js
+++ src/globals.js
@@ -1,13 +1,36 @@
+import { BEthSafePriceValidator } from './BEthSafePriceValidator'
+
 export const globals = {
   env: 'development',
   sentryProjectId: '',
   sentryKey: '',
   ethRpcs: [],
+  deviationBlockOffsets: [],
+  bEthSafePriceValidator: new BEthSafePriceValidator(),
 }
 
-export function setGlobals({ env, sentryProjectId, sentryKey, ethRpcs }) {
+export function setGlobals({
+  env,
+  sentryProjectId,
+  sentryKey,
+  ethRpcs,
+  deviationBlockOffsets,
+  bEthRateLimits,
+  bEthPriceLimits,
+  stEthRateLimits,
+  ethPriceLimits,
+}) {
   globals.env = env || globals.env
   globals.sentryProjectId = sentryProjectId || globals.sentryProjectId
   globals.sentryKey = sentryKey || globals.sentryKey
   globals.ethRpcs = ethRpcs || globals.ethRpcs
+  globals.deviationBlockOffsets =
+    deviationBlockOffsets || globals.deviationBlockOffsets
+  globals.bEthSafePriceValidator = new BEthSafePriceValidator({
+    deviationBlockOffsets,
+    bEthRate: bEthRateLimits,
+    bEthPrice: bEthPriceLimits,
+    stEthRate: stEthRateLimits,
+    ethPrice: ethPriceLimits,
+  })
 }
